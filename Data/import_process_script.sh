aa50cdd348f90dead96486a91458d62720324fd6
Michał Żelasko <zelaskomj@gmail.com>
2023-11-07 20:17:35 +0100

    Data preprocesing and directory structure

---
+++ Data/import_process_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/anat/sub-10159_T1w.json?versionId=mWHa_s6teNNQu1_983mKa1pbABdigX61 -o sub-10159/anat/sub-10159_T1w.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/anat/sub-10159_T1w.nii.gz?versionId=xZH9NrMLEiRpxXIS7D6gg7OEz3g85RlE -o sub-10159/anat/sub-10159_T1w.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/beh/sub-10159_task-stopsignaltraining_beh.json?versionId=T7MotERk31GEAr1ufmMERPQ8vUgh9mq2 -o sub-10159/beh/sub-10159_task-stopsignaltraining_beh.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/beh/sub-10159_task-stopsignaltraining_events.tsv?versionId=Iu9VHoM8aX.bFeKFSIE1zHceoGK9AwQR -o sub-10159/beh/sub-10159_task-stopsignaltraining_events.tsv
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-70086/dwi/sub-70086_dwi.bval?versionId=eZJYZu2Wf9Vn26caaX68LZdNeJ_VVYW5 -o sub-10159/dwi/sub-10159_dwi.bval
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-70052/dwi/sub-70052_dwi.bvec?versionId=QnLEzLmVhJ4l3lSIbnw8EVMM9Ih3htOK -o sub-10159/dwi/sub-10159_dwi.bvec
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/dwi/sub-10159_dwi.json?versionId=iW6eXaBlR4WuP5bbMJR0.q8GROtogRLf -o sub-10159/dwi/sub-10159_dwi.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/dwi/sub-10159_dwi.nii.gz?versionId=L3wsoqPBy1MZqPDW4.Oss1WLBILEsfNj -o sub-10159/dwi/sub-10159_dwi.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-bart_bold.json?versionId=eCxb4FYT4FVMxOlLEJoYQl5jH0PpAwxb -o sub-10159/func/sub-10159_task-bart_bold.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-bart_bold.nii.gz?versionId=oWQd1sWhdFyIBsuUvqQkLqB.CP7wROFZ -o sub-10159/func/sub-10159_task-bart_bold.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-bart_events.tsv?versionId=yr4lhHLWhmwmNkzi3I.yKOfxcpSpVCZ8 -o sub-10159/func/sub-10159_task-bart_events.tsv
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-rest_bold.json?versionId=WgySrQEfyUBwh.wUrbaHsfLzdfw5symP -o sub-10159/func/sub-10159_task-rest_bold.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-rest_bold.nii.gz?versionId=0RVkTZVU1Z3NHCW8e9kVXDB8hqqpNzgl -o sub-10159/func/sub-10159_task-rest_bold.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-scap_bold.json?versionId=FlnuLo9qI71RSyi8LiX6p0uWvLPfx5uX -o sub-10159/func/sub-10159_task-scap_bold.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-scap_bold.nii.gz?versionId=OCj5zF4PEeo43xzhIZW6qw4o3FioIRyb -o sub-10159/func/sub-10159_task-scap_bold.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-scap_events.tsv?versionId=.D_nPJ6Plw59V0SXgWZi3DQG9kqDRXyW -o sub-10159/func/sub-10159_task-scap_events.tsv
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-stopsignal_bold.json?versionId=bXUxveY2XHkneb3NdY3pWyJPsx_OzycO -o sub-10159/func/sub-10159_task-stopsignal_bold.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-stopsignal_bold.nii.gz?versionId=i6BjlRg2jeIBh4vTlFvFrjEW9fvwyDOf -o sub-10159/func/sub-10159_task-stopsignal_bold.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-stopsignal_events.tsv?versionId=AcZrnA0n8NZUmTjzRHr11rwK2yyi_G.q -o sub-10159/func/sub-10159_task-stopsignal_events.tsv
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-taskswitch_bold.json?versionId=Hzog59GNvvPN6z8zf.iCxtP.UmTJpF0E -o sub-10159/func/sub-10159_task-taskswitch_bold.json
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-taskswitch_bold.nii.gz?versionId=1mFYMsEh3hi7KEZDKafinX9n0IoClucN -o sub-10159/func/sub-10159_task-taskswitch_bold.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds000030/sub-10159/func/sub-10159_task-taskswitch_events.tsv?versionId=9sO5Q87yKDO9BIkPl3YLjPQDAwCCcJO6 -o sub-10159/func/sub-10159_task-taskswitch_events.tsv
+export FSLOUTPUTTYPE=NIFTI_GZ
+bet ./sub-10159/anat/sub-10159_T1w.nii.gz ./sub-10159/anat/sub-10159_T1wmask.nii.gz
+bet ./sub-10159/dwi/sub-10159_dwi.nii.gz ./sub-10159/dwi/sub-10159_dwimask.nii.gz
+bet ./sub-10159/func/sub-10159_task-bart_bold.nii.gz ./sub-10159/func/sub-10159_task-bart_boldmask.nii.gz
+bet ./sub-10159/func/sub-10159_task-rest_bold.nii.gz ./sub-10159/func/sub-10159_task-rest_boldmask.nii.gz
+bet ./sub-10159/func/sub-10159_task-scap_bold.nii.gz ./sub-10159/func/sub-10159_task-scap_boldmask.nii.gz
+bet ./sub-10159/func/sub-10159_task-stopsignal_bold.nii.gz ./sub-10159/func/sub-10159_task-stopsignal_boldmask.nii.gz
+bet ./sub-10159/func/sub-10159_task-taskswitch_bold.nii.gz ./sub-10159/func/sub-10159_task-taskswitch_boldmask.nii.gz
